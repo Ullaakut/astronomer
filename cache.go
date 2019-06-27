3696013040b2fe383de606410a3813268884a46d
Ullaakut <brendan.le-glaunec@epitech.eu>
2019-06-27 02:58:48 +0200

    Fix bug which made it impossible to work without cache

--- cache.go
+++ cache.go
@@ -18,12 +18,12 @@ import (
 // supplied request's URL. If found, the file contains a cached copy
 // of the HTTP response. The contents are read into an http.Response
 // object and returned.
-func getCache(c *Context, req *http.Request) (*http.Response, error) {
-	filename := cacheEntryFilename(c, req.URL.String())
+func getCache(ctx *Context, req *http.Request) (*http.Response, error) {
+	filename := cacheEntryFilename(ctx, req.URL.String())
 	pathToCreate := path.Dir(filename)
 
 	if err := os.MkdirAll(pathToCreate, os.ModeDir|0755); err != nil {
-		return nil, fmt.Errorf("unable to create path %q: %v", pathToCreate, err)
+		return nil, err
 	}
 
 	resp, err := readCachedResponse(filename, req)
@@ -31,7 +31,7 @@ func getCache(c *Context, req *http.Request) (*http.Response, error) {
 		if os.IsNotExist(err) {
 			return nil, nil
 		}
-		return nil, fmt.Errorf("unable to read cached response: %v", err)
+		return nil, err
 	}
 
 	return resp, nil
@@ -40,32 +40,32 @@ func getCache(c *Context, req *http.Request) (*http.Response, error) {
 func readCachedResponse(filename string, req *http.Request) (*http.Response, error) {
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return nil, fmt.Errorf("unable to read cached response from file %q: %v", filename, err)
+		return nil, err
 	}
 
 	return http.ReadResponse(bufio.NewReader(bytes.NewBuffer(body)), req)
 }
 
 // putCache puts the supplied http.Response into the cache.
-func putCache(c *Context, req *http.Request, resp *http.Response) error {
+func putCache(ctx *Context, req *http.Request, resp *http.Response) error {
 	defer resp.Body.Close()
 
-	filename := cacheEntryFilename(c, req.URL.String())
+	filename := cacheEntryFilename(ctx, req.URL.String())
 	f, err := os.Create(filename)
 	if err != nil {
-		return fmt.Errorf("unable to create cache file: %v", err)
+		return err
 	}
 
 	if err := resp.Write(f); err != nil {
 		f.Close()
-		return fmt.Errorf("unable to write in cache: %v", err)
+		return err
 	}
 
 	f.Close()
 
 	readResp, err := readCachedResponse(filename, req)
 	if err != nil {
-		return fmt.Errorf("unable to read cached response: %v", err)
+		return err
 	}
 
 	resp.Body = readResp.Body
@@ -74,20 +74,20 @@ func putCache(c *Context, req *http.Request, resp *http.Response) error {
 
 // cacheEntryFilename creates a filename-safe name in a subdirectory
 // of the configured cache dir, with any access token stripped out.
-func cacheEntryFilename(c *Context, url string) string {
-	newURL := strings.Replace(url, fmt.Sprintf("access_token=%s", c.Token), "", 1)
-	return filepath.Join(c.CacheDir, c.Repo, sanitize.BaseName(newURL))
+func cacheEntryFilename(ctx *Context, url string) string {
+	newURL := strings.Replace(url, fmt.Sprintf("access_token=%s", ctx.Token), "", 1)
+	return filepath.Join(ctx.CacheDir, ctx.Repo, sanitize.BaseName(newURL))
 }
 
 // clearEntry clears a specified cache entry.
-func clearEntry(c *Context, url string) error {
-	filename := cacheEntryFilename(c, url)
+func clearEntry(ctx *Context, url string) error {
+	filename := cacheEntryFilename(ctx, url)
 	return os.Remove(filename)
 }
 
 // Clear clears all cache entries for the repository specified in the
 // fetch context.
-func Clear(c *Context) error {
-	filename := filepath.Join(c.CacheDir, c.Repo)
+func Clear(ctx *Context) error {
+	filename := filepath.Join(ctx.CacheDir, ctx.Repo)
 	return os.RemoveAll(filename)
 }
